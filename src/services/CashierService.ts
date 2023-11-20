753786d272cfd720c1353caab22d775259a742e6
Ana Julia <anajuliassdev@gmail.com>
2023-11-20 18:02:06 -0300

    service de cashier

--- src/services/CashierService.ts
+++ src/services/CashierService.ts
@@ -1,1 +1,44 @@
-export default class CashierService {}
+import Cashier from "../dto/CashierDto";
+
+export default class CashierService {
+    public static async getAll(): Promise<Cashier[]> {
+        return fetch (`${import.meta.env.VITE_API_URL}/cashiers/all`).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(); 
+            }
+        });
+    }
+
+    public static async getById(id: number): Promise<Cashier> {
+        return fetch(`${import.meta.env.VITE_API_URL}/cashiers/${id}`).then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error(); 
+            }
+        }); 
+    }
+
+    public static async getByIsOpen(isOpen: boolean): Promise<Cashier[]> {
+        return fetch(`${import.meta.env.VITE_API_URL}/cashiers/isOpen/${isOpen}`).then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error(); 
+            }
+        });
+    }
+    
+    public static async getTotal(id: number):Promise<number> {
+        return fetch(`${import.meta.env.VITE_API_URL}/cashiers/totalSales/${id}`).then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error(); 
+            }
+        });
+    }
+    
+}

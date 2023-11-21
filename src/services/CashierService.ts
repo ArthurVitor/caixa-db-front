0d93cb4369fcbe3d09190d6a82b8514f32036bd0
Ana Julia <anajuliassdev@gmail.com>
2023-11-21 14:39:41 -0300

    caixas do banco de dados listados em cashierPage

--- src/services/CashierService.ts
+++ src/services/CashierService.ts
@@ -2,17 +2,23 @@ import Cashier from "../dto/CashierDto";
 
 export default class CashierService {
     public static async getAll(): Promise<Cashier[]> {
-        return fetch (`${import.meta.env.VITE_API_URL}/cashiers/all`).then(response => {
+        return fetch (`http://localhost:8080/api/cashiers/all`).then(response => {
             if (response.ok) {
-                return response.json();
+                console.log(response);
+                return response.json().then((cashiers) => 
+                cashiers.map((cashier: any) => ({
+                   ...cashier, 
+                   openDate: CashierService.getFormattedDate(cashier.openDate)
+                }))
+                );
             } else {
-                throw new Error(); 
+                throw new Error("Erro service"); 
             }
         });
     }
 
     public static async getById(id: number): Promise<Cashier> {
-        return fetch(`${import.meta.env.VITE_API_URL}/cashiers/${id}`).then(response => {
+        return fetch(`http://localhost:8080/api/cashiers/${id}`).then(response => {
             if(response.ok) {
                 return response.json();
             } else {
@@ -22,7 +28,7 @@ export default class CashierService {
     }
 
     public static async getByIsOpen(isOpen: boolean): Promise<Cashier[]> {
-        return fetch(`${import.meta.env.VITE_API_URL}/cashiers/isOpen/${isOpen}`).then(response => {
+        return fetch(`http://localhost:8080/api/cashiers/isOpen/${isOpen}`).then(response => {
             if(response.ok) {
                 return response.json();
             } else {
@@ -32,13 +38,24 @@ export default class CashierService {
     }
     
     public static async getTotal(id: number):Promise<number> {
-        return fetch(`${import.meta.env.VITE_API_URL}/cashiers/totalSales/${id}`).then(response => {
+        return fetch(`http://localhost:8080/api/cashiers/totalSales/${id}`).then(response => {
             if(response.ok) {
                 return response.json();
             } else {
-                throw new Error(); 
+                console.error('Erro ao obter o total:',response.statusText);
+                throw new Error("Erro service"); 
             }
         });
     }
+
+
+    public static getFormattedDate(string: string): Date {
+        let spaceSplit = string.split(" ");
+
+        let [day, month, year] = spaceSplit[0].split("/");
+        let [hour, minute, second] = spaceSplit[1].split(":");
+    
+        return new Date(Number(year), Number(month), Number(day), Number(hour), Number(minute), Number(second));
+      }
     
 }

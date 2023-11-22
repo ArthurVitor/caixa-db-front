46ea7e512f435804fefa760ab65e248ede9eb6ee
yandv <allanmxr@gmail.com>
2023-11-22 00:14:21 -0300

    feat: resolvido conflito no merge

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import { ProductPage } from "./components/product/ProductPage";
 import { ItemSellPage } from "./components/itemsell/ItemSellPage";
 
 import "./App.css";
-import CashierDetailsPage from "./components/cashier/CashierDetailsPage";
-import CreateSalePage from "./components/sale/CreateSalePage";
 import CreateProductPage from "./components/product/CreateProductPage";
+import CreateSalePage from "./components/sale/create/CreateSalePage";
+import CashierDetailsPage from "./components/cashier/CashierDetailsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(

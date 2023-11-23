3b7e3a36722988ba6c5382dd9873506030b95f10
nicofercavv-dev <nicoleferreira281@gmail.com>
2023-11-23 16:50:44 -0300

    Implementa variavel de ambiente no Product Service

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -2,7 +2,7 @@ import Product from "../dto/ProductDto";
 
 export default class ProductService {
   public static async getProducts(): Promise<Product[]> {
-    return fetch("http://localhost:8080/api/products").then((response) =>
+    return fetch(`${import.meta.env.VITE_API_URL}/products`).then((response) =>
       response.json()
     )
     .catch(err => 
@@ -11,14 +11,14 @@ export default class ProductService {
   }
 
   public static async getProductById(id: number): Promise<Product> {
-    return fetch(`http://localhost:8080/api/product/${id}`).then(response => 
+    return fetch(`${import.meta.env.VITE_API_URL}/product/${id}`).then(response => 
       response.json()
     )
     .catch(err => console.error(err))
   }
 
   public static async getProductByBarcode(barcode: string): Promise<Product> {
-    return fetch(`http://localhost:8080/api/product?barcode=${barcode}`).then(response => 
+    return fetch(`${import.meta.env.VITE_API_URL}/product?barcode=${barcode}`).then(response => 
       response.json()
     )
     .catch(err => console.error(err))
@@ -26,7 +26,7 @@ export default class ProductService {
 
   public static async createProduct(product: object): Promise<Product> {
     const data: string = JSON.stringify(product)
-    return fetch("http://localhost:8080/api/product", {
+    return fetch(`${import.meta.env.VITE_API_URL}/product`, {
       method: "POST",
       body: data,
       headers: { "Content-type": "application/json" }
@@ -37,7 +37,7 @@ export default class ProductService {
 
   public static async editProduct(id: number, data: object): Promise<string> {
     const putData: string = JSON.stringify(data);
-    return fetch(`http://localhost:8080/api/product/${id}`, {
+    return fetch(`${import.meta.env.VITE_API_URL}/product/${id}`, {
       method: "PUT",
       body: putData,
       headers: { "Content-type": "application/json" }
@@ -47,7 +47,7 @@ export default class ProductService {
   }
 
   public static async deleteProduct(id: number): Promise<number | void> {
-    return fetch(`http://localhost:8080/api/product/${id}`, {
+    return fetch(`${import.meta.env.VITE_API_URL}/product/${id}`, {
       method: "DELETE"
     }).then(response =>
       response.status
